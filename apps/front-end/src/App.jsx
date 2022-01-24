da94a9c4cb7468a5b7dd9a587a95440f7f7bab2e
Maor Lachish <89803221+mlachish@users.noreply.github.com>
2022-01-24 20:37:33 +0200

    stored states with context

--- apps/front-end/src/App.jsx
+++ apps/front-end/src/App.jsx
@@ -1,47 +1,23 @@
 import { socket } from './socket.js'
-import { useEffect, useState } from 'react'
+import { useContext, useEffect } from 'react'
 
 import EnterGame from './pages/EnterGame'
 import WaitingRoom from './pages/WaitingRoom'
 import GameRoom from './pages/GameRoom'
 import EndGame from './pages/EndGame'
+import { TicTacToeContext } from './store'
 
 function App() {
-  const [playState, setPlayState] = useState(localStorage.userToken ? null : 'enter')
-  const [gameStatus, setGameStatus] = useState(null)
+	const { playState } = useContext(TicTacToeContext)
 
-  useEffect(() => {
-    socket.on('status', (gameStatus) => {
-      setGameStatus(gameStatus)
-      setPlayState(gameStatus.status) 
-    })
-
-    socket.on('connect', () => setPlayState('identify'))
-
-    socket.on('disconnect', () => {
-      socket.connect()
-    })
-  }, [])
-
-  useEffect(() => {
-    if (playState === 'identify') {
-      socket.emit('identify', localStorage.userToken)
-    }
-  }, [playState])
-
-  function setPlayerToken(playerId) {
-    localStorage.setItem('userToken', playerId)
-    setPlayState('identify')
-  }
-
-  return (
-    <div className="App">
-      {playState === 'enter' && <EnterGame onEnterGame={setPlayerToken}/>}
-      {playState === 'waiting' && <WaitingRoom />}
-      {playState === 'running' && <GameRoom gameStatus={gameStatus}/>}
-      {playState === 'ended' && <EndGame />}
-    </div>
-  )
+	return (
+		<div className='App'>
+			{playState === 'enter' && <EnterGame />}
+			{playState === 'waiting' && <WaitingRoom />}
+			{playState === 'running' && <GameRoom />}
+			{playState === 'ended' && <EndGame />}
+		</div>
+	)
 }
 
 export default App
